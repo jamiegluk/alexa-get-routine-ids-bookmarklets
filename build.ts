be2ac683ed4c339212d8732d76d6f826d714070e
Jamie Gerrard Lievesley <jamie@jamiegl.co.uk>
2020-10-22 23:15:53 +0100

    HTML escape and styles

--- build.ts
+++ build.ts
@@ -1,3 +1,4 @@
+import { htmlEscape } from "escape-goat";
 import { readFileSync, writeFileSync } from "fs";
 
 import * as packageJson from "./package.json";
@@ -15,10 +16,10 @@ let pageText = readFileSync("template.html").toString();
 pageText = pageText.replace(/{{URI_US}}/g, usURI);
 pageText = pageText.replace(/{{URI_UK}}/g, ukURI);
 
-pageText = pageText.replace(/{{TITLE}}/g, escape(packageJson.title));
-pageText = pageText.replace(/{{AUTHOR}}/g, escape(packageJson.author));
-pageText = pageText.replace(/{{DESCRIPTION}}/g, escape(packageJson.description));
-pageText = pageText.replace(/{{KEYWORDS}}/g, escape(packageJson.keywords.join(" ")));
-pageText = pageText.replace(/{{VERSION}}/g, escape(packageJson.version));
+pageText = pageText.replace(/{{TITLE}}/g, htmlEscape(packageJson.title));
+pageText = pageText.replace(/{{AUTHOR}}/g, htmlEscape(packageJson.author));
+pageText = pageText.replace(/{{DESCRIPTION}}/g, htmlEscape(packageJson.description));
+pageText = pageText.replace(/{{KEYWORDS}}/g, htmlEscape(packageJson.keywords.join(" ")));
+pageText = pageText.replace(/{{VERSION}}/g, htmlEscape(packageJson.version));
 
 writeFileSync(".build/index.html", pageText);
